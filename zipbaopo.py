05123250c5a6c6a7ef386d1da6c3c51c73a2a01f
ck65 <2294458237@qq.com>
2018-09-24 20:45:27 +0800

    爆破final

--- zipbaopo.py
+++ zipbaopo.py
@@ -1,20 +1,30 @@
-from zipfile import *
+import zipfile
+import optparse
+from threading import Thread
 
-def extractFile(File, password):
+def extractFile(File,password):
     try:
-        File.extractall(pwd = password)
-        return password
+        File.extractall(pwd=password)
+        print '[+]Passcode = ' + password + '\n'
     except:
-        return
+        pass
 
 def main():
-    zFile = ZipFile('evil.zip')
-    passFile = open('dictionary.txt')
-    for line in passFile.readlines():
-        password = line.strip('\n')
-        guess = extractFile(zFile,password)
-        if guess:
-            print '[+] Password = ' + password + '\n'
-            exit(0)
+    parser = optparse.OptionParser("usage%prog " + "-f <zipfile> -d <dictionary>")
+    parser.add_option('-f', dest = 'zname', type = 'string', help = 'specify zip file')
+    parser.add_option('-d', dest = 'dname', type = 'string', help = 'specify dictionary file ')
+    (options, args) = parser.parse_args()
+    if (options.zname == None) | (options.dname == None):
+        print parser.usage
+        exit(0)
+    else:
+        zname = options.zname
+        dname = options.dname
+        zFile = zipfile.ZipFile(zname)
+        passFile = open(dname)
+        for line in passFile.readlines():
+            password = line.strip('\n')
+            t = Thread(target=extractFile, args=(zFile,password))
+            t.start()
 if __name__ == '__main__':
     main()
