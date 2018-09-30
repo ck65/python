670fdf5e868a583d57a41ab2ebbfd5a6cf3ab4bc
ck65 <2294458237@qq.com>
2018-09-30 17:41:05 +0800

    NMAP

---
+++ SSH.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# @Time: 2018/9/30 14:38
+import nmap
+import optparse
+
+def nmapScan(tgtHost, tgtPort):
+    nmScan = nmap.PortScanner()
+    nmScan.scan(tgtHost.tgtPort)
+    state = nmScan[tgtHost]['tcp'][int(tgtPort)]['state']
+    print '[*] '+tgtHost+' tcp/'+tgtPort +" "+state
+
+def main():
+    parser = optparse.OptionParser('Usage %prog -H <target host> -p <target prot>')
+    parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
+    parser.add_option('-p', dest='tgtPort', type='string', help = 'specify target port[s] spearated by comma')
+    (options, args) = parser.parse_args()
+    tgtHost = options.tgtHost
+    tgtPorts = str(options.tgtPort).split(', ')
+    if (tgtHost == None) | (tgtPorts[0]==None):
+        print parser.usage
+        exit(0)
+    for tgtPort in tgtPorts:
+        nmapScan(tgtHost, tgtPort)
+
+if __name__ == '__main__':
+     main()
